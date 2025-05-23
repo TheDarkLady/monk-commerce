6fa6dcf2b1bc45d11eec76f3e1c52bbd89268e3d
TheDarkLady <lakshmichaithanya13@gmail.com>
2025-05-23 18:19:18 +0530

    api integration is pending

--- src/components/AddProductsDesktop.jsx
+++ src/components/AddProductsDesktop.jsx
@@ -32,12 +32,12 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { MdModeEdit } from "react-icons/md";
 import { IoIosClose } from "react-icons/io";
-import { CiSearch } from "react-icons/ci";
+
 
 const AddProductsDesktop = () => {
     const [count, setCount] = useState(1);
     const [showDiscounts, setShowDiscounts] = useState({});
-    const [showPopup, setShowPopup] =  useState(false)
+    const [showPopup, setShowPopup] = useState(false)
     // const [searchTerm, setSearchTerm] = useState("");
     // const [debouncedSearch, setDebouncedSearch] = useState("");
     // const [selectedProductMap, setSelectedProductMap] = useState({});
@@ -55,7 +55,7 @@ const AddProductsDesktop = () => {
             });
         }
     };
-    
+
 
     // const productData = useMemo(() => [
     //     {
@@ -89,83 +89,84 @@ const AddProductsDesktop = () => {
 
     return (
         <>
-        <div className='relative'>
-             <div>
-                {[...Array(count)].map((_, index) => (
-                    <div key={index} className='flex flex-row justify-center items-center max-w-[700px] gap-4'>
-                        <div>
-                            <RxDragHandleDots2 />
-                        </div>
-                        <div>
-                            {index + 1}.
-                        </div>
-                        <div className="relative w-full flex flex-row justify-center items-center  pt-2 pb-2">
-                            <Button variant="ghost" className="absolute right-3 text-[#0003] h-[20px] w-[20px]" onClick={() => {
-                                setShowPopup(true)
-                                console.log("showpopup clicked")
+            <div className='relative'>
+                <div>
+                    {[...Array(count)].map((_, index) => (
+                        <div key={index} className='flex flex-row justify-center items-center max-w-[700px] gap-4'>
+                            <div>
+                                <RxDragHandleDots2 />
+                            </div>
+                            <div>
+                                {index + 1}.
+                            </div>
+                            <div className="relative w-full flex flex-row justify-center items-center  pt-2 pb-2">
+                                <Button variant="ghost" className="absolute right-3 text-[#0003] h-[20px] w-[20px]" onClick={() => {
+                                    setShowPopup(true)
+                                    console.log("showpopup clicked")
                                 }}>
-                                <MdModeEdit />
-                            </Button>
-                            <input
-                                type="input"
-                                placeholder="Select Product"
-                                className="w-full p-1.5 pl-10 border border-[#0003] rounded focus:outline-none focus:border-[#000]  placeholder:text-[#0003] shadow"
-                            />
-                        </div>
-                        <div>
-                            {!showDiscounts[index] ? (
-                                <Button
-                                    className="bg-[#008060] text-[#F5F5F5] font-semibold text-[14px] px-4 py-2 rounded-md cursor-pointer hover:bg-[#008060]"
-                                    onClick={() =>
-                                        setShowDiscounts(prev => ({ ...prev, [index]: true }))
-                                    }
-                                >
-                                    Add Discount
+                                    <MdModeEdit />
                                 </Button>
-                            ) : (
-                                <div className='flex flex-row items-center justify-start gap-2'>
-                                    <Input
-                                        type="text"
-                                        placeholder='Discount'
-                                        className='p-2 w-[100px]'
-                                    />
-                                    <Select>
-                                        <SelectTrigger className="w-[100px]">
-                                            <SelectValue placeholder="" />
-                                        </SelectTrigger>
-                                        <SelectContent>
-                                            <SelectItem value="% off">% off</SelectItem>
-                                            <SelectItem value="Flat off">Flat off</SelectItem>
-                                        </SelectContent>
-                                    </Select>
-                                </div>
-                            )}
-                        </div>
-                        <div>
-                            {count > 1 && (
-                                <Button variant="ghost" onClick={() => decreaseCount(index)}>
-                                    <IoIosClose className='text-[#0003]' />
-                                </Button>
-                            )}
+                                <input
+                                    type="input"
+                                    placeholder="Select Product"
+                                    className="w-full p-1.5 pl-10 border border-[#0003] rounded focus:outline-none focus:border-[#000]  placeholder:text-[#0003] shadow"
+                                />
+                            </div>
+                            <div>
+                                {!showDiscounts[index] ? (
+                                    <Button
+                                        className="bg-[#008060] text-[#F5F5F5] font-semibold text-[14px] px-4 py-2 rounded-md cursor-pointer hover:bg-[#008060]"
+                                        onClick={() =>
+                                            setShowDiscounts(prev => ({ ...prev, [index]: true }))
+                                        }
+                                    >
+                                        Add Discount
+                                    </Button>
+                                ) : (
+                                    <div className='flex flex-row items-center justify-start gap-2'>
+                                        <Input
+                                            type="text"
+                                            placeholder='Discount'
+                                            className='p-2 w-[100px]'
+                                        />
+                                        <Select>
+                                            <SelectTrigger className="w-[100px]">
+                                                <SelectValue placeholder="" />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectItem value="% off">% off</SelectItem>
+                                                <SelectItem value="Flat off">Flat off</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </div>
+                                )}
+                            </div>
+                            <div>
+                                {count > 1 && (
+                                    <Button variant="ghost" onClick={() => decreaseCount(index)}>
+                                        <IoIosClose className='text-[#0003]' />
+                                    </Button>
+                                )}
+                            </div>
+
                         </div>
 
+                    ))}
+
+
+                    <div className='flex flex-row justify-end'>
+                        <Button
+                            className="text-[#008060] bg-transparent border border-[#008060] font-semibold text-[14px] px-4 py-2 rounded-md mt-4 hover:bg-[#008060] hover:text-[#fff] cursor-pointer"
+                            onClick={increaseCount}
+                        >
+                            Add Product
+                        </Button>
                     </div>
-
-                ))}
-
-
-                <div className='flex flex-row justify-end'>
-                    <Button
-                        className="text-[#008060] bg-transparent border border-[#008060] font-semibold text-[14px] px-4 py-2 rounded-md mt-4 hover:bg-[#008060] hover:text-[#fff] cursor-pointer"
-                        onClick={increaseCount}
-                    >
-                        Add Product
-                    </Button>
                 </div>
             </div>
-            <SearchPopup id="searchpopup"  style={{display : showPopup ? "block" : "none"}} className="h-full w-full bg-[#0001] absolute top-0 left-0 z-50"/>
-           
-        </div>
+                {showPopup &&
+                    <SearchPopup id="searchpopup"  />
+                }
         </>
     )
 }
--- src/components/SearchPopup.jsx
+++ src/components/SearchPopup.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { Button } from "@/components/ui/button"
 
 const SearchPopup = () => {
   return (
-    <div>
-      
+    <div className="h-full w-full bg-[#0003] absolute top-0 left-0 z-50  flex flex-col justify-center items-center">
+      <div className='relative flex flex-col justify-start items-start w-full max-w-[700px] bg-[#fff] h-[80%] gap-0'>
+        <div className='flex flex-col justify-start items-start px-8 py-3 w-full  border-b border-[#0000001a]'>
+          <h1>Select Products</h1>
+        </div>
+        <div className='relative flex flex-col justify-start items-start px-8 py-3 w-full  border-b border-[#0000001a] gap-0'>
+          <input
+            type="input"
+            placeholder="Search Product"
+            className="w-full p-1.5 pl-10 border border-[#0003] rounded focus:outline-none focus:border-[#000]  placeholder:text-[#0003] shadow"
+          />
+          <CiSearch className="absolute left-10 top-1/2 -translate-y-1/2 text-[#0003] h-[20px] w-[20px]" />
+        </div>
+        <div className='absolute bottom-0 flex flex-row justify-between items-center px-8 py-3 w-full'>
+          <h1>Select Products</h1>
+          <div className='w-[50%] flex flex-row justify-end items-center gap-3'>
+            <Button
+              className="text-[#0006] bg-transparent border border-[#0006] font-semibold text-[14px] px-4 py-2 rounded-md cursor-pointer"
+            >
+              Cancle
+            </Button>
+            <Button
+              className="text-[#008060] bg-transparent border border-[#008060] font-semibold text-[14px] px-4 py-2 rounded-md hover:bg-[#008060] hover:text-[#fff] cursor-pointer"
+            >
+              Add
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
